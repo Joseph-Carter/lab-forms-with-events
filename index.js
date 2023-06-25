c324cae93792d91bb995614193016df79ca990f9
Joseph Carter <josephc@Josephs-Air.home>
2023-06-25 19:38:56 -0400

    finished lab with Anthony and Alexander

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Code your solution!")
+const h1 = document.querySelector("h1")
+
+let ul = document.createElement("ul")
+
+let form = document.createElement("from")
+
+let input = document.createElement("input")
+
+let body = document.querySelector("body")
+
+h1.textContent = "My To-Dos"
+form.append(input)
+input.type = "text"
+input.placeholder = "Type a todo"
+
+body.appendChild(form)

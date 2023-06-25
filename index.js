f7c37e0ed1d75c4f09c8768b274791540d30bdd0
Joseph Carter <josephc@Josephs-Air.home>
2023-06-25 19:41:17 -0400

    finished lab with Anthony and Alexander

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-const h1 = document.querySelector("h1")
+const h1 = document.querySelector("h1");
 
-let ul = document.createElement("ul")
+let input = document.createElement("input");
 
-let form = document.createElement("from")
+let body = document.querySelector("body");
 
-let input = document.createElement("input")
+let errorMessage = document.createElement("p");
+errorMessage.style.color = "red";
 
-let body = document.querySelector("body")
+h1.textContent = "My To-Dos";
+let ul = document.createElement("ul");
+let form = document.createElement("form");
 
-h1.textContent = "My To-Dos"
-form.append(input)
-input.type = "text"
-input.placeholder = "Type a todo"
+form.append(input);
+input.type = "text";
+input.placeholder = "Type a todo";
+input.name = "todo";
 
-body.appendChild(form)
+body.appendChild(form);
+body.appendChild(errorMessage);
+
+let button = document.createElement("button");
+button.type = "submit";
+button.textContent = "Submit";
+form.append(button);
+
+function generateTodo(todo) {
+  let li = document.createElement("li");
+  li.style.cursor = "pointer";
+  li.innerText = todo;
+  ul.appendChild(li);
+  li.addEventListener("click", (event) => {
+    if (li.style.textDecoration == "line-through") {
+      li.style.textDecoration = null;
+    } else {
+      li.style.textDecoration = "line-through";
+    }
+    if (li.style.color === "green") {
+      li.style.color = "black";
+    } else {
+      li.style.color = "green";
+    }
+  });
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let todo = event.target.todo.value;
+  if (!todo) {
+    errorMessage.textContent = "Error. Todo cannot be empty";
+  } else {
+    errorMessage.textContent = "";
+    generateTodo(todo);
+  }
+
+  form.reset();
+});
+
+body.appendChild(ul);
